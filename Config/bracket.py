0c15a6b99bccfe1eacfd70c6be31f291da1e6219
yash goel <yashgoel236437@gmail.com>
2022-02-05 23:14:47 +0530

    v3

---
+++ Config/bracket.py
@@ -0,0 +1,29 @@
+import configparser
+
+def bracket(loc,sec,key):
+    config = configparser.RawConfigParser()
+    config.read(loc)
+    temp = []
+    # store dict info to parse into list
+    temp_list = dict(config.items(sec))[key]
+    for i in temp_list.split(','):
+        value = ""
+        flag = 0
+        for j in i:
+            if j in ['(',')','+','-','.']:
+                value = value+"["+j+"]"
+                flag = 0
+            elif j == " " and flag == 0:
+                value = value+".*"
+                flag = 1
+            else:
+                value = value+j
+                flag = 0
+        temp.append(value)
+    config.set(sec, key+"1", ",".join(temp))
+    with open(loc, 'w') as configfile:
+        config.write(configfile)
+    print(temp)
+
+
+bracket("config2021.cfg","ITR_6","temp")
--- data/Parse.py
+++ data/Parse.py
@@ -23,76 +23,41 @@ class Itrparser:
         config.read('Config/config'+year+".cfg")
         # example ITR_3
         section = "ITR_"+str(itr)
-        # all the page no and table at which info is present
-        self.pageno = dict(config.items(section))["pages"].split(",")
-        self.tablep = dict(config.items(section))["tablep"].split(",")
-
-        # page data
-        for i in self.pageno:
-            temp = {}
-            # store dict info to parse into list
-            temp_dict = dict(config.items(section+"_"+i))
-            for key in temp_dict.keys():
-                temp[key] = temp_dict[key].split(",")
-            # dict to its corresponding pages
-            self.configuration[i] = temp
+        temp = {}
+        # store dict info to parse into list
+        temp_dict = dict(config.items(section))
+        for key in temp_dict.keys():
+            temp[key] = temp_dict[key].split(",")
+        # dict to its corresponding pages
+        self.configuration = temp
 
         # table data
-        for i in self.tablep:
-            if i == '':
-                break
-            temp = {}
-            # store dict info to parse into list
-            temp_dict = dict(config.items(section + "_" + i + 't'))
-            for key in temp_dict.keys():
-                temp[key] = temp_dict[key].split(",")
-            # dict to its corresponding table
-            self.configuration[i+'t'] = temp
+        # for i in self.tablep:
+        #     if i == '':
+        #         break
+        #     temp = {}
+        #     # store dict info to parse into list
+        #     temp_dict = dict(config.items(section + "_" + i + 't'))
+        #     for key in temp_dict.keys():
+        #         temp[key] = temp_dict[key].split(",")
+        #     # dict to its corresponding table
+        #     self.configuration[i+'t'] = temp
 
-    def load_data(self):
+    def tableparse(self, tables, key):
 
-        itr = -1
-        year = ["2021-22", "2020-21", "2019-20"]
-        with pdfplumber.open(self.loc) as pdf:
-            pages = pdf.pages[0].extract_text()
+        # input : table : nested list containing string
+        #         key : column of the table whose first element from row to choose
+        # output : value at first row of that column
 
-            for page in pages.split('\n'):
+        key = re.compile(r".*{}.*".format(key.lower()))
+        for table in tables:
+            for j in range(len(table)):
+                for k in range(len(table[j])):
+                    if table[j][k] != None:
+                        if key.match(table[j][k].lower()):
+                            return table[j+1][k]
 
-                # check for the year in the docx
-                if type(year) != str:
-                    for i in year:
-                        temp = page.replace(" ", "")
-                        if temp.find(i) != -1:
-                            year = i.split("-")[0]
-
-                # check for the itr no in the docx
-                if itr == -1:
-                    if page.find("INDIAN INCOME TAX RETURN ACKNOWLEDGEMENT") != -1:
-                        itr = 0
-                    elif re.compile(r"ITR.?3").match(page):
-                        itr = 3
-                    elif re.compile(r"ITR.?4").match(page):
-                        itr = 4
-                    elif re.compile(r"ITR.?5").match(page):
-                        itr = 5
-                    elif re.compile(r"ITR.?6").match(page):
-                        itr = 6
-                    else:
-                        itr = -1
-                if type(year) == str and itr != -1:
-                    break
-            # call function and produce config dict
-            self.configdict(itr, year)
-            self.pages['0'] = pages
-
-            # loop through page after 1st page
-            for i in self.pageno[1:]:
-                self.pages[i] = pdf.pages[int(i)].extract_text()                              # load pages in dictionary
-
-            self.itr = itr
-            self.year = int(year)
-
-    def page_all(self, page, no):
+    def page_all(self, page, idx, table):
 
         #   input  : string page
         #   output : dict containing the detail according to config file
@@ -102,31 +67,29 @@ class Itrparser:
 
         # get config from the dict
 
-        start = self.configuration[no]["start"]
-        last = self.configuration[no]["last"]
-        key = self.configuration[no]["key"]
-        ftype = self.configuration[no]["ftype"]
-        d_type = self.configuration[no]["dtype"]
-        columns = self.configuration[no]["columns"]
-        columne = self.configuration[no]["columne"]
+        start = self.configuration["start"]
+        last = self.configuration["last"]
+        key = self.configuration["key"]
+        ftype = self.configuration["ftype"]
+        d_type = self.configuration["dtype"]
+        columns = self.configuration["columns"]
+        columne = self.configuration["columne"]
 
         # split page into list
 
         page_split = page.split("\n")
         data = {}
-        idx = 0
 
         # initialize re function
 
         if d_type[idx] == "string":
-            regex_compile = re.compile(r".*{}.*".format(start[idx]))
+            regex_compile = re.compile(r".*{}.*".format(start[idx].lower()))
         else:
-            regex_compile = re.compile(r".*{}.*{}.*\d".format(start[idx], last[idx]))
+            regex_compile = re.compile(r".*{}.*{}.*\d".format(start[idx].lower(), last[idx].lower()))
 
         # loop through all the line of the page
         for i in range(len(page_split)):
-
-            if regex_compile.match(page_split[i]):                                         # line match the re
+            if regex_compile.match(page_split[i].lower()):                                         # line match the re
                 # in order to find the number
                 if d_type[idx] == "no":
                     *_, l = page_split[i].split(' ')
@@ -142,64 +105,113 @@ class Itrparser:
                         if last_name.lower().startswith("prop"):
                             last_name = " ".join(last_name.split(" ")[2:])
                         # Name,company name
-                        data.update({"Full Name": first_name, "Company_name": last_name})
+                        data.update({"Full Name": first_name, "cmpny_name": last_name})
                     # all the other itr
                     else:
+                        flag = 1
                         tempi = 0
-                        if ftype[idx] == "up":
+                        if ftype[idx] == "same":
+                            tempi = i
+                        elif ftype[idx] == "up":
                             tempi = i+1
                         else:
-                            tempi = i
-                        if columne[idx] == columns[idx]:
-                            data[key[idx]] = (page_split[tempi].split(" "))[int(columne[idx])]
-                        else:
-                            s = 0
-                            col = int(columne[idx])
-                            if col == 0:
-                                col = None
-                            temp = ' '.join((page_split[tempi].split(" "))[int(columns[idx]):col])
-                            for s in range(len(temp)):
-                                if temp[s].isnumeric():
-                                    s -= 1
-                                    break
-                            data[key[idx]] = temp[:s+1]
+                            flag = 0
+                            data[key[idx]] = self.tableparse(table, start[idx])
+
+                        if flag == 1:
+                            if columne[idx] == columns[idx]:
+                                data[key[idx]] = (page_split[tempi].split(" "))[int(columne[idx])]
+                            else:
+                                s = 0
+                                col = int(columne[idx])
+                                if col == 0:
+                                    col = None
+                                temp = ' '.join((page_split[tempi].split(" "))[int(columns[idx]):col])
+                                for s in range(len(temp)):
+                                    if temp[s].isnumeric():
+                                        s -= 1
+                                        break
+                                data[key[idx]] = temp[:s+1]
                 idx += 1
                 # no of import elements
                 if idx < len(start):
                     if d_type[idx] == "string":
-                        regex_compile = re.compile(r".*{}.*".format(start[idx]))
+                        regex_compile = re.compile(r".*{}.*".format(start[idx].lower()))
                     else:
-                        regex_compile = re.compile(r".*{}.*{}.*\d".format(start[idx], last[idx]))
+                        regex_compile = re.compile(r".*{}.*{}.*\d".format(start[idx].lower(), last[idx].lower()))
                 else:
-                    break
-        return data
+                    return data, -1
+        return data, idx
 
-    def tableparse(self):
-        data_dict = {}
+    def parse_data(self):
+        value = {}
+        idx = 0
+        itr = -1
+        year = ["2021-22", "2020-21", "2019-20"]
         with pdfplumber.open(self.loc) as pdf:
-            for i in self.tablep:
-                table_l = []
-                table = pdf.pages[int(i)].extract_table()
-                row = int(self.configuration[i+'t']["row"][0])
-                column = int(self.configuration[i+'t']["column"][0])
-                key = self.configuration[i+'t']["key"][0]
-                table = table[row]
-                for j in table:
-                    if type(j) == str:
-                        if len(j) > 0:
-                            table_l.append(j)
-                data_dict[key] = table_l[column]
-        return data_dict
+            page = pdf.pages[0].extract_text()
+
+            for line in page.split('\n'):
+
+                # check for the year in the docx
+                if type(year) != str:
+                    for i in year:
+                        temp = line.replace(" ", "")
+                        if temp.find(i) != -1:
+                            year = i.split("-")[0]
+
+                # check for the itr no in the docx
+                if itr == -1:
+                    if line.find("INDIAN INCOME TAX RETURN ACKNOWLEDGEMENT") != -1:
+                        itr = 0
+                    elif re.compile(r"ITR.?3").match(line):
+                        itr = 3
+                    elif re.compile(r"ITR.?4").match(line):
+                        itr = 4
+                    elif re.compile(r"ITR.?5").match(line):
+                        itr = 5
+                    elif re.compile(r"ITR.?6").match(line):
+                        itr = 6
+                    else:
+                        itr = -1
+                if type(year) == str and itr != -1:
+                    break
+
+            self.itr = itr
+            self.year = int(year)
+            # call function and produce config dict
+            self.configdict(itr, year)
+            dictdata, idx = self.page_all(page, idx, None)
+            value.update(dictdata)
+
+            ftype = self.configuration["ftype"]
+            pageadd = self.configuration["pageadd"]
+            i = 1
+            # loop through page after 1st page
+            while i < 30:
+                table = None
+                i = i+int(pageadd[idx])
+                pageadd[idx] = 0
+                page = pdf.pages[i].extract_text()                             # load pages in dictionary
+                if ftype[idx] == 'table':
+                    table = pdf.pages[i].extract_tables()
+                dictdata, idx = self.page_all(page, idx, table)
+                value.update(dictdata)
+                if idx == -1:
+                    break
+                i = i+1
+            print(len(value))
+            print(value)
 
     def main(self):
         value = {}
-        self.load_data()
-        for i in self.pageno:
-            value.update(self.page_all(self.pages[i], i))
-        if len(self.tablep[0]) > 0:
-            value.update(self.tableparse())
-        print(value)
-        print(len(value))
+        self.parse_data()
+        # for i in self.pageno:
+        #     value.update(self.page_all(self.pages[i], i))
+        # if len(self.tablep[0]) > 0:
+        #     value.update(self.tableparse())
+        # print(value)
+        # print(len(value))
 
 
 def parsepdf(loc):
