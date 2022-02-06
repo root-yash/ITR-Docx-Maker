ff4d9577e30e131e10d252e4e1382dea406d146e
yash goel <yashgoel236437@gmail.com>
2022-02-06 20:06:23 +0530

    req.txt

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ from gui import open_file
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 main=Tk()
-main.size
+#main.size
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
 
-    browse=Button(main, text="browse", command=open_file)
-    l1 = Label(main, text="hello world")
-    l2 = Label(main, text="hi india")
-    l1.grid(row=6, column=0)
-    l2.grid(row=5, column=1)
+    browse = Button(main, text="browse", command=open_file)
     browse.grid(row=1)
     main.mainloop()
 
